386d2a18f4dd62655c6eba17e167d484f0f2735e
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-15 20:20:33 +0100

    Add Collection Utils class

---
+++ spec/Raekke/Utils/Collection.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace spec\Raekke\Utils;
+
+use PHPSpec2\ObjectBehavior;
+
+class Collection extends ObjectBehavior
+{
+    function it_should_behave_like_an_array()
+    {
+        $this->shouldHaveType('Countable');
+        $this->shouldHaveType('IteratorAggregate');
+    }
+
+    function it_should_allow_to_recieve_remove_and_get_elements()
+    {
+        $this->has('key0')->shouldReturn(false);
+        $this->get('key0')->shouldReturn(null);
+
+        $this->set('key0', 'value0');
+
+        $this->has('key0')->shouldReturn(true);
+        $this->get('key0')->shouldReturn('value0');
+
+        $this->remove('key0');
+
+        $this->has('key0')->shouldReturn(false);
+        $this->get('key0')->shouldReturn(null);
+    }
+
+    /**
+     * @param ArrayIterator $iterator
+     */
+    function it_should_allow_to_get_all_values_at_once($iterator)
+    {
+        $this->all()->shouldReturnAnInstanceOf('ArrayIterator');
+
+        $this->set('key0', 'value0');
+
+        $iterator = $this->all();
+        $iterator->count()->shouldReturn(1);
+
+        $iterator = $this->getIterator();
+        $iterator->count()->shouldReturn(1);
+    }
+}
---
+++ src/Raekke/Utils/Collection.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Raekke\Utils;
+
+/**
+ * Object Oriented Array.
+ *
+ * @package Raekke
+ */
+class Collection implements \Countable, \IteratorAggregate
+{
+    protected $elements = array();
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public function get($key)
+    {
+        return $this->has($key) ? $this->elements[$key] : null;
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     */
+    public function set($key, $value)
+    {
+        $this->elements[$key] = $value;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function has($key)
+    {
+        return isset($this->elements[$key]);
+    }
+
+    /**
+     * @param string $key
+     */
+    public function remove($key)
+    {
+        unset($this->elements[$key]);
+    }
+
+    /**
+     * @return array
+     */
+    public function all()
+    {
+        return new \ArrayIterator($this->elements);
+    }
+
+    /**
+     * @return integer
+     */
+    public function count()
+    {
+        return count($this->elements);
+    }
+
+    /**
+     * @return Iterator
+     */
+    public function getIterator()
+    {
+        return $this->all();
+    }
+}
