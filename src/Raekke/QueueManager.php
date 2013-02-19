deb3a31062da939138db6724ca980fc82f357dbd
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-19 16:02:22 +0100

    Add QueueManager and PHPUnit test

---
+++ src/Raekke/QueueManager.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Raekke;
+
+use Raekke\Message\MessageInterface;
+use Raekke\Queue\Queue;
+use Raekke\Serializer\Serializer;
+use Raekke\Util\ArrayCollection;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Contains a collection of queues.
+ *
+ * @package Raekke
+ */
+class QueueManager implements \IteratorAggregate, \ArrayAccess, \Countable
+{
+    protected $queues;
+    protected $connection;
+    protected $serializer;
+
+    public function __construct(
+        Connection $connection,
+        Serializer $serializer = null,
+        EventDispatcherInterface $dispatcher = null
+    ) {
+        $this->queues     = new ArrayCollection;
+        $this->connection = $connection;
+        $this->serializer = $serializer ?: new Serializer;
+        $this->dispatcher = $dispatcher ?: new EventDispatcher;
+
+        $this->get('failed');
+    }
+
+    public function get($queueName)
+    {
+        if ($this->queues->containsKey($queueName)) {
+            return $this->queues->get($queueName);
+        }
+
+        $this->queues->set($queueName, $queue = $this->createQueueObject($queueName));
+
+        return $queue;
+    }
+
+    public function all()
+    {
+        foreach ($this->connection->all('queues') as $queue) {
+            $this->get($queue);
+        }
+
+        return $this->queues;
+    }
+
+    public function has($queueName)
+    {
+        if ($this->queues->containsKey($queueName)) {
+            return true;
+        }
+
+        return (boolean) $this->connection->has('queues', $queueName) ? true : false;
+    }
+
+    public function count()
+    {
+        return $this->connection->count('queues');
+    }
+
+    public function enqueue(MessageInterface $message)
+    {
+        $this->get($message->getQueue())->push($message);
+    }
+
+    public function remove($queueName)
+    {
+        if (!$this->has($queueName)) {
+            return false;
+        }
+
+        $this->get($queueName)->close();
+
+        $this->queues->remove($queueName);
+
+        return true;
+    }
+
+    public function getIterator()
+    {
+        return $this->all()->getIterator();
+    }
+
+    public function offsetSet($queueName, $value)
+    {
+        throw new \BadMethodCallException('"offsetSet" is not supported.');
+    }
+
+    public function offsetGet($queueName)
+    {
+        return $this->get($queueName);
+    }
+
+    public function offsetExists($queueName)
+    {
+        return $this->has($queueName);
+    }
+
+    public function offsetUnset($queueName)
+    {
+        return $this->remove($queueName);
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    public function getSerializer()
+    {
+        return $this->serializer;
+    }
+
+    public function getDispatcher()
+    {
+        return $this->dispatcher;
+    }
+
+    protected function createQueueObject($queueName)
+    {
+        $queue = new Queue($queueName, $this);
+        $queue->attach();
+
+        return $queue;
+    }
+}
---
+++ tests/Raekke/Tests/QueueManagerTest.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Raekke\Tests;
+
+use Raekke\QueueManager;
+
+class QueueManagerTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->connection = $this->getMockBuilder('Raekke\Connection')
+            ->disableOriginalConstructor()->getMock();
+
+        $this->manager = new QueueManager($this->connection);
+    }
+
+    public function testItEnqueuesMessages()
+    {
+        $message = $this->getMock('Raekke\Message\MessageInterface');
+        $message->expects($this->once())->method('getQueue')->will($this->returnValue('queue'));
+
+        $queue = $this->getMockBuilder('Raekke\Queue\Queue')->disableOriginalConstructor()
+            ->getMock();
+
+        $queue->expects($this->once())->method('push')->with($this->equalTo($message));
+
+        $manager = $this->getMockBuilder('Raekke\QueueManager')->disableOriginalConstructor()
+            ->setMethods(array('get'))->getMock();
+
+        $manager->expects($this->once())->method('get')->with($this->equalTo('queue'))
+            ->will($this->returnValue($queue));
+
+        $manager->enqueue($message);
+    }
+
+    public function testItSavesQueueObjects()
+    {
+        $this->connection->expects($this->once())->method('insert')
+            ->with($this->equalTo('queues'), $this->equalTo('queue'));
+
+        $queue = $this->manager->get('queue');
+
+        $this->assertSame($queue, $this->manager->get('queue'));
+    }
+
+    public function testRemoveClosesQueue()
+    {
+        $queue = $this->manager->get('queue');
+
+        $this->assertTrue($this->manager->has('queue'));
+        $this->assertTrue($this->manager->remove('queue'));
+        $this->assertTrue($queue->isClosed());
+
+        $this->assertFalse($this->manager->remove('queue-nonexistant'));
+    }
+
+    public function testItLazyCreatesQueuesAndAttaches()
+    {
+        $this->connection->expects($this->once())->method('insert')->with($this->equalTo('queues'), $this->equalTo('queue'));
+
+        $this->assertInstanceOf('Raekke\Queue\Queue', $this->manager->get('queue'));
+        $this->assertSame($this->manager, $this->manager->get('queue')->getManager());
+    }
+
+    public function testItHaveASerializerAndEventDispatcher()
+    {
+        $this->assertInstanceOf('Raekke\Serializer\Serializer', $this->manager->getSerializer());
+        $this->assertInstanceOf('Symfony\Component\EventDispatcher\EventDispatcherInterface', $this->manager->getDispatcher());
+
+        $manager = new QueueManager($this->connection,
+            $serializer = $this->getMock('Raekke\Serializer\Serializer'),
+            $dispatcher = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface'));
+
+        $this->assertSame($serializer, $manager->getSerializer());
+        $this->assertSame($dispatcher, $manager->getDispatcher());
+    }
+
+    public function testArrayAccess()
+    {
+        $queue = $this->manager->get('queue');
+        $this->assertSame($queue, $this->manager['queue']);
+
+        $this->assertFalse(isset($this->manager['queue2']));
+        $this->assertTrue(isset($this->manager['queue']));
+
+        unset($this->manager['queue']);
+
+        $this->assertTrue($queue->isClosed());
+
+        try {
+            $this->manager['queue3'] = 'something';
+
+            $this->fail('Setting on QueueManager is expected to raise an exception.');
+        } catch (\Exception $e) {
+            $this->assertInstanceOf('BadMethodCallException', $e);
+        }
+    }
+
+    public function testItsCountable()
+    {
+        $this->assertInstanceOf('Countable', $this->manager);
+        $this->assertInstanceOf('IteratorAggregate', $this->manager);
+
+        $this->connection->expects($this->once())->method('count')->with($this->equalTo('queues'))
+            ->will($this->returnValue(4));
+
+        $this->assertCount(4, $this->manager);
+    }
+
+    public function testItGetsAllQueues()
+    {
+        $this->connection->expects($this->exactly(2))->method('all')->with($this->equalTo('queues'))
+            ->will($this->returnValue(array('queue1', 'queue2')));
+
+        $all = $this->manager->all();
+
+        $this->assertInstanceOf('Raekke\Util\ArrayCollection', $all);
+        $this->assertCount(3, $all);
+        $this->assertTrue($all->containsKey('failed'));
+
+        $this->assertInstanceOf('IteratorAggregate', $this->manager);
+        $this->assertEquals($all->getIterator(), $this->manager->getIterator());
+    }
+}
