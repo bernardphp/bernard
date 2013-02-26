91d1763fa76c9d6fea17b3b9aa091477611192da
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-26 18:33:45 +0100

    fail example every 10%

--- example/EchoTimeService.php
+++ example/EchoTimeService.php
@@ -6,6 +6,10 @@ class EchoTimeService
 {
     public function onEchoTime(DefaultMessage $message)
     {
+        if (rand(0, 10) === 7) {
+            throw new \RuntimeException('I failed because rand was 7');
+        }
+
         echo "Time is: " . $message->time . "\n";
 
         sleep(1);
