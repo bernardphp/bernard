5b51214a16bf76b3b0f03b104f4c3ba6b770f18a
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-26 18:17:56 +0100

    add signal handling

--- src/Raekke/Consumer.php
+++ src/Raekke/Consumer.php
@@ -12,6 +12,7 @@ class Consumer implements ConsumerInterface
 {
     protected $failed;
     protected $services;
+    protected $shutdown = false;
 
     /**
      * @param ServiceResolverInterface $services
@@ -30,7 +31,15 @@ class Consumer implements ConsumerInterface
      */
     public function consume(Queue $queue)
     {
+        // register with monitoring object class thing
+
+        $this->registerSignalHandlers();
+
         while (true) {
+            if ($this->shutdown) {
+                break;
+            }
+
             if (null === $wrapper = $queue->dequeue()) {
                 continue;
             }
@@ -49,5 +58,18 @@ class Consumer implements ConsumerInterface
                 $this->failed->enqueue($wrapper);
             }
         }
+
+        // Unregister with consumer monitoring thing
+    }
+
+    public function shutdown()
+    {
+        $this->shutdown = true;
+    }
+
+    public function registerSignalHandlers()
+    {
+        pcntl_signal(\SIGTERM, array($this, 'shutdown'));
+        pcntl_signal(\SIGINT, array($this, 'shutdown'));
     }
 }
