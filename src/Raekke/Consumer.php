8ffa9e554fa303361e4751ba8314cd7e58a7544c
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-26 12:23:42 +0100

    prematurely added eventdispatcher

--- src/Raekke/Consumer.php
+++ src/Raekke/Consumer.php
@@ -3,23 +3,12 @@
 namespace Raekke;
 
 use Raekke\Queue\Queue;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * @package Consumer
  */
 class Consumer implements ConsumerInterface
 {
-    protected $dispatcher;
-
-    /**
-     * @param EventDispatcherInterface $dispatcher
-     */
-    public function __construct(EventDispatcherInterface $dispatcher)
-    {
-        $this->dispatcher = $dispatcher;
-    }
-
     /**
      * {@inheritDoc}
      */
