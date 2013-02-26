1a654b6f0fe6f8aaf9798865cd4b26d9321fd3fa
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-26 13:56:36 +0100

    Add completely untested stuff to show what direction it is taking

--- src/Raekke/Consumer.php
+++ src/Raekke/Consumer.php
@@ -3,16 +3,66 @@
 namespace Raekke;
 
 use Raekke\Queue\Queue;
+use Raekke\Consumer\Job;
+use Raekke\Message\MessageInterface;
 
 /**
  * @package Consumer
  */
 class Consumer implements ConsumerInterface
 {
+    protected $failed;
+
+    /**
+     * @param Queue $failed Failed messages will be enqueued on this.
+     */
+    public function __construct(Queue $failed)
+    {
+        $this->failed = $failed;
+    }
+
     /**
      * {@inheritDoc}
      */
     public function consume(Queue $queue)
     {
+        while (true) {
+            if (null === $wrapper = $queue->dequeue(10)) {
+                continue;
+            }
+
+            try {
+                // Try and invoke the job, if it exists with \ReflectionException the error
+                // is most likely that the method cannot be called on the service object.
+                // It should then instantly be marked as failed.
+                $job = new Job(new ServiceObject, $wrapper->getMessage());
+                $job->invoke();
+            } catch (\Exception $e) {
+                $failed = false;
+
+                if ($e instanceof \ReflectionException) {
+                    $this->failed->enqueue($wrapper);
+                    $failed = true;
+                }
+
+                if ($wrapper->getRetries() < 5) {
+                    $failed = true;
+                }
+
+                if (!$failed) {
+                    // Increment retries and requeue
+                    $wrapper->incrementRetries();
+                    $queue->enqueue($wrapper);
+                }
+            }
+        }
+    }
+}
+
+class ServiceObject
+{
+    public function onSendNewsletter(MessageInterface $message)
+    {
+        sleep(10);
     }
 }
