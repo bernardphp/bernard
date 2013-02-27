f108b3274d7d8bf9701d2635a603b7038c54072a
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-27 11:53:34 +0100

    Display errors in example

--- example/bootstrap.php
+++ example/bootstrap.php
@@ -8,6 +8,9 @@ use Raekke\QueueFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+ini_set('display_erros', 1);
+error_reporting(E_ALL);
+
 $jmsSerializer = JMS\Serializer\SerializerBuilder::create()
     ->addMetadataDir(__DIR__ . '/../src/Raekke/Resources/serializer', 'Raekke')
     ->build();
