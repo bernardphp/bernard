060873cc8ebf0c792c7c1407ad77b5a4721afef3
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-27 07:32:23 +0100

    Add console command

---
+++ src/Raekke/Command/ConsumeCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Raekke\Command;
+
+use Raekke\Consumer;
+use Raekke\QueueFactory;
+use Raekke\ServiceResolverInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+/**
+ * @package Raekke
+ */
+class ConsumeCommand extends \Symfony\Component\Console\Command\Command
+{
+    protected $services;
+    protected $queues;
+
+    public function __construct(
+        ServiceResolverInterface $services,
+        QueueFactory $queues
+    ) {
+        $this->services = $services;
+        $this->queues = $queues;
+
+        parent::__construct();
+    }
+
+    public function configure()
+    {
+        $this
+            ->setName('raekke:consume')
+            ->addOption('max-retries', null, InputOption::VALUE_OPTIONAL, 'Number of times a message will be requeued before marked as failed.', null)
+            ->addArgument('queue', InputArgument::INPUT_REQUIRED, 'Name of queue that will be consumed.')
+        ;
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $consumer = new Consumer($this->services, $this->queues->create('failed'));
+        $consumer->consume($this->queues->create($input->getArgument('queue')), array(
+            'max_retries' => $input->getOption('max-retries'),
+        ));
+    }
+}
