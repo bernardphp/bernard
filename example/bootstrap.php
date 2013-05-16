7dc6bdada17f01519193b372d3f2db9113facbe0
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-05-16 18:33:48 +0200

    Use symfony serializer in examples as it is the simplest one

--- example/bootstrap.php
+++ example/bootstrap.php
@@ -1,22 +1,21 @@
 <?php
 
-use JMS\Serializer\SerializerBuilder;
 use Predis\Client;
 use Bernard\Connection\PredisConnection;
-use Bernard\Serializer\JMSSerializer;
+use Bernard\Serializer\SymfonySerializer;
+use Bernard\Symfony\EnvelopeNormalizer;
 use Bernard\QueueFactory\PersistentFactory;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 ini_set('display_erros', 1);
 error_reporting(E_ALL);
 
-$jmsSerializer = SerializerBuilder::create()
-    ->addMetadataDir(__DIR__ . '/../src/Bernard/Resources/serializer', 'Bernard')
-    ->build();
+$serializer = new SymfonySerializer(new Serializer(array(new EnvelopeNormalizer), array(new JsonEncoder)));
 
 $connection = new PredisConnection(new Client(null, array(
     'prefix' => 'bernard:',
 )));
-$serializer = new JMSSerializer($jmsSerializer);
 $queues = new PersistentFactory($connection, $serializer);
