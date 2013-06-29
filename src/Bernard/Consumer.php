29b106af9f52f593fcd329d9536a91d847fdb42f
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-06-29 17:59:45 +0200

    Add test for consumer

--- src/Bernard/Consumer.php
+++ src/Bernard/Consumer.php
@@ -35,11 +35,50 @@ class Consumer
         $this->bind();
         $this->configure($options);
 
-        while (microtime(true) < $this->options['max-runtime'] && !$this->shutdown) {
-            if ($envelope = $queue->dequeue()) {
-                $this->invoke($envelope, $queue, $failed);
-            }
+        while ($this->tick($queue, $failed)) {
+            // NO op
         }
+    }
+
+    /**
+     * Returns true do indicate it should be run again or false to indicate 
+     * it should not be run again.
+     *
+     * @param Queue $queue
+     * @param Queue|null $failed
+     * @return boolean
+     */
+    public function tick(Queue $queue, Queue $failed = null)
+    {
+        if ($this->shutdown) {
+            return false;
+        }
+
+        if (microtime(true) > $this->options['max-runtime']) {
+            return false;
+        }
+
+        if (!$envelope = $queue->dequeue()) {
+            return true;
+        }
+
+        try {
+            $invocator = $this->services->resolve($envelope);
+            $invocator->invoke();
+        } catch (Exception $e) {
+            $this->fail($envelope, $e, $queue, $failed);
+        }
+
+        return true;
+    }
+
+    /**
+     * @param array $options
+     */
+    public function configure(array $options)
+    {
+        $this->options = array_filter($options) + $this->options;
+        $this->options['max-runtime'] += microtime(true);
     }
 
     /**
@@ -48,19 +87,6 @@ class Consumer
     public function shutdown()
     {
         $this->shutdown = true;
-    }
-
-    /**
-     * @param Envelope $envelope
-     */
-    protected function invoke(Envelope $envelope, Queue $queue, Queue $failed = null)
-    {
-        try {
-            $invocator = $this->services->resolve($envelope->getMessage());
-            $invocator->invoke();
-        } catch (Exception $e) {
-            $this->fail($envelope, $e, $queue, $failed);
-        }
     }
 
     /**
@@ -79,15 +105,6 @@ class Consumer
         if ($failed) {
             $failed->enqueue($envelope);
         }
-    }
-
-    /**
-     * @param array $options
-     */
-    protected function configure(array $options)
-    {
-        $this->options = array_filter($options) + $this->options;
-        $this->options['max-runtime'] = microtime(true) + $this->options['max-runtime'];
     }
 
     /**
--- tests/Bernard/Tests/ConsumerTest.php
+++ tests/Bernard/Tests/ConsumerTest.php
@@ -6,14 +6,61 @@ use Bernard\Consumer;
 
 class ConsumerTest extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        $this->resolver = $this->getMock('Bernard\ServiceResolver');
+        $this->consumer = new Consumer($this->resolver);
+    }
+
     public function testShutdown()
     {
-        $consumer = new Consumer($this->getMock('Bernard\ServiceResolver'));
-        $consumer->shutdown();
+        $queue = $this->getMock('Bernard\Queue');
 
-        $r = new \ReflectionProperty($consumer, 'shutdown');
-        $r->setAccessible(true);
+        $this->consumer->shutdown();
 
-        $this->assertTrue($r->getValue($consumer));
+        $this->assertFalse($this->consumer->tick($queue));
+    }
+
+    public function testMaxRuntime()
+    {
+        $queue = $this->getMock('Bernard\Queue');
+
+        // Make sure max runtime is a looongtime in the past
+        $this->consumer->configure(array(
+            'max-runtime' => -1 * PHP_INT_MAX,
+        ));
+
+        $this->assertFalse($this->consumer->tick($queue));
+    }
+
+    public function testNoEnvelopeInQueue()
+    {
+        $this->resolver->expects($this->never())->method('resolve');
+
+        $queue = $this->getMock('Bernard\Queue');
+        $queue->expects($this->once())->method('dequeue')->will($this->returnValue(null));
+
+        $this->assertTrue($this->consumer->tick($queue));
+    }
+
+    public function testEnvelopeWillBeInvoked()
+    {
+        $envelope = $this->getMockBuilder('Bernard\Message\Envelope')->disableOriginalConstructor()
+            ->getMock();
+        $envelope->expects($this->never())->method('incrementRetries');
+
+        $invocator = $this->getMockBuilder('Bernard\ServiceResolver\Invocator')->disableOriginalConstructor()
+            ->getMock();
+
+        $invocator->expects($this->once())->method('invoke');
+
+        $this->resolver->expects($this->once())->method('resolve')
+            ->with($this->equalTo($envelope))->will($this->returnValue($invocator));
+
+        $queue = $this->getMock('Bernard\Queue');
+        $queue->expects($this->once())->method('dequeue')->will($this->returnValue($envelope));
+        $queue->expects($this->never())->method('enqueue');
+
+        $this->assertTrue($this->consumer->tick($queue));
     }
 }
