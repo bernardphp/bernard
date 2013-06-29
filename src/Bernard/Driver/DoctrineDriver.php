7dc91d80baa8f5e8810f69312c2d41ac08ea1a63
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-06-29 12:31:36 +0200

    Intial DoctrineDriver

---
+++ src/Bernard/Driver/DoctrineDriver.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Bernard\Driver;
+
+use Doctrine\DBAL\Connection;
+
+/**
+ * Driver supporting Doctrine DBAL
+ *
+ * @package Bernard
+ */
+class DoctrineDriver implements \Bernard\Driver
+{
+    protected $connection;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function listQueues()
+    {
+        return $this->connection->fetchColumn('SELECT queue FROM messages GROUP BY queue');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createQueue($queueName)
+    {
+        // noop
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function countMessages($queueName)
+    {
+        return $this->connection->fetchColumn('SELECT COUNT(id) FROM messages');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function pushMessage($queueName, $message)
+    {
+        $queue = $queueName;
+
+        $this->connection->insert('messages', compact('queue', 'message'), array(
+            'string',
+            'string',
+        ));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function popMessage($queueName, $interval = 5)
+    {
+        $this->connection->beginTransaction();
+
+        try {
+            list($id, $message) = $this->connection->fetchArray('SELECT id, message FROM messages WHERE queue = :queue', array(
+                ':queue' => $queueName,
+            ));
+
+            $this->connection->delete('messages', $id);
+
+            $this->commit();
+        } catch (\Exception $e) {
+            $this->connection->rollback();
+
+            throw $e;
+        }
+
+        if (isset($message)) {
+            return $message;
+        }
+
+        // Sleep 100 ms between each select.
+        usleep(100);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function peekQueue($queueName, $index = 0, $limit = 20)
+    {
+        $statement = $this->connection->prepare('SELECT message FROM messages LIMIT :index, :limit');
+        $statement->execute(array(
+            ':index' => $index,
+            ':limit' => $index + $limit,
+        ));
+
+        return $statement->fetchAll(PDO::FETCH_COLUMN);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function removeQueue($queueName)
+    {
+        $this->connection->delete('messages', array('queue' => $queueName));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function info()
+    {
+        $params = $this->connection->getParams();
+
+        return array(
+            'host'     => $params['host'],
+            'port'     => $params['port'],
+            'database' => $params['dbname'],
+        );
+    }
+}
