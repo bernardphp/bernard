0fb485df1e80cb09141628a47dc60d70df02139d
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-07-23 09:00:08 +0200

    AppEngine

---
+++ tests/Bernard/Tests/Driver/AppEngineDriverTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bernard\Tests\Driver;
+
+use Bernard\Driver\AppEngineDriver;
+use google\appengine\api\taskqueue\PushTask;
+
+class AppEngineDriverTest extends \PHPUnit_Framework_TestCase
+{
+    public static function setUpBeforeClass()
+    {
+        // Very ugly hack! But AppEngine SDK isnt available outside appengine
+        // environment.
+        class_alias('Bernard\Tests\Fixtures\PushTask', 'google\appengine\api\taskqueue\PushTask');
+    }
+
+    public function setUp()
+    {
+        $this->driver = new AppEngineDriver(array(
+            'send-newsletter' => '/url_endpoint',
+        ));
+    }
+
+    public function testItQueuesPushTask()
+    {
+        $this->driver->pushMessage('send-newsletter', 'message');
+
+        $this->assertEquals(new PushTask('/url_endpoint', array('message' => 'message')), PushTask::$messages[0]);
+    }
+
+    public function testThrowsExceptionOnInvalidQueueMap()
+    {
+        $this->setExpectedException('InvalidArgumentException', 'Queue "import-users" cannot be resolved to an endpoint.');
+
+        $this->driver->pushMessage('import-users', '');
+    }
+
+    public function testListQueues()
+    {
+        $this->assertEquals(array('/url_endpoint' => 'send-newsletter'), $this->driver->listQueues());
+    }
+}
