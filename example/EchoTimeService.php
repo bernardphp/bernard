1a2bbd54f1060d2f10f512df85a17f9238b03126
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-09-06 13:31:57 +0200

    dont echo in example service

--- example/EchoTimeService.php
+++ example/EchoTimeService.php
@@ -6,11 +6,9 @@ class EchoTimeService
 {
     public function onEchoTime(DefaultMessage $message)
     {
-        if (rand(0, 10) === 7) {
+        if (rand(0, 10) == 7) {
             throw new \RuntimeException('I failed because rand was 7');
         }
-
-        echo "Time is: " . $message->time . "\n";
 
         usleep(100);
     }
