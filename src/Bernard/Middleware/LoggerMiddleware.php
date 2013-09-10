36efd6c2dd0bce850b959cd67230056e188bf04a
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-09-10 12:52:24 +0200

    Simplified Logger

--- src/Bernard/Middleware/LoggerMiddleware.php
+++ src/Bernard/Middleware/LoggerMiddleware.php
@@ -16,13 +16,11 @@ class LoggerMiddleware implements Middleware
     /**
      * @param Middleware $next
      * @param LoggerInterface $logger
-     * @param string $usage Either "consumer" or "producer"
      */
-    public function __construct(Middleware $next, LoggerInterface $logger, $usage = 'consumer')
+    public function __construct(Middleware $next, LoggerInterface $logger)
     {
         $this->next = $next;
         $this->logger = $logger;
-        $this->usage = $usage;
     }
 
     /**
@@ -30,28 +28,18 @@ class LoggerMiddleware implements Middleware
      */
     public function call(Envelope $envelope)
     {
-        if ($this->usage == 'producer') {
-            $this->logger->info('[Bernard] Produced "{message}" for "{queue}".', array(
-                'name' => $envelope->getName(),
-                'queue' => $envelope->getMessage()->getQueue(),
-            ));
-
-            return;
-        }
-
-        $this->logger->info('[Bernard] Processing message "{queue}" since {time} [{name}]".', array(
+        $context = array(
             'queue' => $envelope->getMessage()->getQueue(),
             'name' => $envelope->getName(),
-            'time' => time(),
-        ));
+        );
 
         try {
+            $this->logger->info('[Bernard] Processing message "{name}" from "{queue}".', $context);
             $this->next->call($envelope);
-        } catch (\Exception $e) {
-            $this->logger->error('[Bernard] Received exception while processing "{name}".', array(
-                'name' => $envelope->getName(),
-                'exeception' => $e,
-            ));
+
+        } catch (\Exception $exception) {
+            $context += compact('exception');
+            $this->logger->error('[Bernard] Received exception while processing "{name}".', $context);
 
             throw $e;
         }
