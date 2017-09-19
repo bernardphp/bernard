dd724733d678cc00db25de97f7035f4b480a80b3
Martin Reinfandt <reinfandt@z-em.de>
2017-09-19 06:09:27 +0200

    add psr container router

---
+++ src/Router/PsrContainerAwareRouter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bernard\Router;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * @package Bernard\Router
+ */
+class PsrContainerAwareRouter extends SimpleRouter
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @param ContainerInterface $container
+     * @param array              $receivers
+     */
+    public function __construct(ContainerInterface $container, array $receivers = [])
+    {
+        $this->container = $container;
+
+        parent::__construct($receivers);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function get($name)
+    {
+        $serviceId = parent::get($name);
+
+        return $this->container->get($serviceId);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function accepts($receiver)
+    {
+        return $this->container->has($receiver);
+    }
+}
