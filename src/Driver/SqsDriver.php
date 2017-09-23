28e5b261d5566ac57e40758ad9ec4f6b98b3d467
Menno Holtkamp <holtkamp@users.noreply.github.com>
2017-09-23 21:22:29 +0200

    Amazon SQS automatic queue creation (#327)
    * AWS SQS FIFO Queue support
    
    * Only use FifoQueue Attribute when dealing with a Queue that ends with '.fifo'
    
    * Add testItCreatesFifoQueue()
    
    * Add testItPushesMessagesToFifoQueue()
    
    * Use queueExists() to check whether queue exists by analysing exception code

--- src/Driver/SqsDriver.php
+++ src/Driver/SqsDriver.php
@@ -14,6 +14,7 @@ use Aws\Sqs\SqsClient;
 class SqsDriver extends AbstractPrefetchDriver
 {
     const AWS_SQS_FIFO_SUFFIX = '.fifo';
+    const AWS_SQS_EXCEPTION_BAD_REQUEST = 400;
 
     protected $sqs;
     protected $queueUrls;
@@ -58,25 +59,54 @@ class SqsDriver extends AbstractPrefetchDriver
      * {@inheritdoc}
      *
      * @link http://docs.aws.amazon.com/aws-sdk-php/v3/api/api-sqs-2012-11-05.html#createqueue
+     *
+     * @return void
+     *
+     * @throws SqsException
      */
     public function createQueue($queueName)
     {
+        if($this->queueExists($queueName)){
+            return;
+        }
+
+        $parameters = [
+            'QueueName' => $queueName,
+        ];
+
+        if($this->isFifoQueue($queueName)) {
+            $parameters['Attributes'] = [
+                'FifoQueue' => 'true',
+            ];
+        }
+
+        $result = $this->sqs->createQueue($parameters);
+
+        $this->queueUrls[$queueName] = $result['QueueUrl'];
+    }
+
+    /**
+     * @param string $queueName
+     *
+     * @return bool
+     *
+     * @throws SqsException
+     */
+    private function queueExists($queueName)
+    {
         try {
             $this->resolveUrl($queueName);
-        } catch (\InvalidArgumentException $e) {
-            $parameters = [
-                'QueueName' => $queueName,
-            ];
-
-            if($this->isFifoQueue($queueName)) {
-                $parameters['Attributes'] = [
-                    'FifoQueue' => 'true',
-                ];
+            return true;
+        } catch (\InvalidArgumentException $exception) {
+            return false;
+        } catch (SqsException $exception) {
+            if ($previousException = $exception->getPrevious()) {
+                if ($previousException->getCode() === self::AWS_SQS_EXCEPTION_BAD_REQUEST) {
+                    return false;
+                }
             }
-            
-            $result = $this->sqs->createQueue($parameters);
 
-            $this->queueUrls[$queueName] = $result['QueueUrl'];
+            throw $exception;
         }
     }
 
