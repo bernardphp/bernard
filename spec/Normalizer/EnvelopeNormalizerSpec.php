cf9423322690533f45a300de6852658145ed3eae
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2018-02-16 01:10:37 +0100

    Improve specs

--- spec/Normalizer/EnvelopeNormalizerSpec.php
+++ spec/Normalizer/EnvelopeNormalizerSpec.php
@@ -4,6 +4,7 @@ namespace spec\Bernard\Normalizer;
 
 use Bernard\Envelope;
 use Bernard\Message;
+use Bernard\Normalizer\EnvelopeNormalizer;
 use Normalt\Normalizer\AggregateNormalizer;
 use PhpSpec\ObjectBehavior;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
@@ -11,16 +12,24 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class EnvelopeNormalizerSpec extends ObjectBehavior
 {
-    function it_is_a_normalizer_and_denormailzer()
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(EnvelopeNormalizer::class);
+    }
+
+    function it_is_a_normalizer()
     {
         $this->shouldImplement(NormalizerInterface::class);
+    }
+
+    function it_is_adenormailzer()
+    {
         $this->shouldImplement(DenormalizerInterface::class);
     }
 
-    function it_normalizes_envelope_and_delegates_message_to_aggregate(AggregateNormalizer $aggregate)
+    function it_normalizes_envelope_and_delegates_message_to_aggregate(Message $message, AggregateNormalizer $aggregate)
     {
-        $message = new Message\PlainMessage('message');
-        $envelope = new Envelope($message);
+        $envelope = new Envelope($message->getWrappedObject());
         $time = time();
 
         $aggregate->normalize($message)->willReturn([
@@ -30,7 +39,7 @@ class EnvelopeNormalizerSpec extends ObjectBehavior
         $this->setAggregateNormalizer($aggregate);
 
         $this->normalize($envelope)->shouldReturn([
-            'class' => Message\PlainMessage::class,
+            'class' => get_class($message->getWrappedObject()),
             'timestamp' => $time,
             'message' => ['key' => 'value'],
         ]);
@@ -48,7 +57,7 @@ class EnvelopeNormalizerSpec extends ObjectBehavior
             'message' => ['key' => 'value'],
         ];
 
-        $envelope = $this->denormalize($normalized, Envelope::class);
+        $envelope = $this->denormalize($normalized, null);
         $envelope->shouldHaveType(Envelope::class);
         $envelope->getMessage()->shouldReturn($message);
         $envelope->getTimestamp()->shouldReturn(1337);
